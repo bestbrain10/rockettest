3e15fb4230726bb830ea1c47afedc7355e05a013
Bestbrain Igbaji Livinus <bestbrain10@gmail.com>
2020-09-29 17:09:59 +0100

    switch to vercel servelss format

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
-import express from "express";
-import bodyParser from "body-parser";
 import { outputToString, analyze, extractInput } from "./util";
-
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+import { NowRequest, NowResponse } from '@vercel/node';
 // define a route handler for the default
-app.post('/analyze/tasks', (req: express.Request, res: express.Response) => {
+
+export default () => (req: NowRequest, res: NowResponse, _: any) => {
     const { input } = req.body;
-    if(!input) {
+    if (!input) {
         res.status(400).send(`
             No input recieved, send an input using the 'input' body parameter
         `);
@@ -21,32 +17,7 @@ app.post('/analyze/tasks', (req: express.Request, res: express.Response) => {
             )
         );
         res.send(response);
-    } catch( e) {
+    } catch (e) {
         res.status(400).send(e);
     }
-
-});
-
-app.all('**', (req: express.Request, res: express.Response, _: any) => {
-    return res.json({
-        status: 'online, use POST /analyze/tasks to analyze tasks',
-        date: (new Date()).toString()
-    });
-});
-
-app.use((err: Error | any, req: express.Request, res: express.Response, _: any) => {
-    if (err.toString() === '[object Object]') {
-        res.status(400).send(err);
-    } else {
-        res.status(400).send(err.toString());
-    }
-});
-
-
-// start the express server
-export default () => {
-    app.listen(3000, () => {
-        // tslint:disable-next-line:no-console
-        console.log(`server started at http://localhost:${3000}`);
-    });
 };
